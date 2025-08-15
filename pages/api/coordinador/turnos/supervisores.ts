acc8f67d72055c4729b083dc0f3c8c276bddb3a1
githubprojecter <github@projectercompany.com>
2025-08-14 19:30:40 -0600

    Commit Correctivo v1.0.0: Se modifico correctamente la api de supervisores

--- pages/api/coordinador/turnos/supervisores.ts
+++ pages/api/coordinador/turnos/supervisores.ts
@@ -29,6 +29,7 @@ export default requireRole(['Coordinador'])(async (req: NextApiRequestWithUser, 
   // console.log(sup)
   const items = sup.map(s => ({
     id: s.id,
+    codigo: s.codigo,
     nombre: [s.user?.nombre, s.user?.apellidoPaterno, s.user?.apellidoMaterno].filter(Boolean).join(' ') || `Supervisor #${s.id}`,
     idFirebase: s.user.idFirebase,
     telefono: s.user?.telefono ?? null
